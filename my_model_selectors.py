33596288cdbef3f521f60bb755048f2b3f125716
lucko515 <luka.anicin@gmail.com>
2017-07-29 18:20:36 +0200

    ASL Recognizer application

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -12,21 +12,20 @@ class ModelSelector(object):
     '''
     base class for model selection (strategy design pattern)
     '''
-
-    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
-                 n_constant=3,
+    
+    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str, n_constant=3,
                  min_n_components=2, max_n_components=10,
                  random_state=14, verbose=False):
-        self.words = all_word_sequences
-        self.hwords = all_word_Xlengths
-        self.sequences = all_word_sequences[this_word]
-        self.X, self.lengths = all_word_Xlengths[this_word]
-        self.this_word = this_word
-        self.n_constant = n_constant
-        self.min_n_components = min_n_components
-        self.max_n_components = max_n_components
-        self.random_state = random_state
-        self.verbose = verbose
+                self.words = all_word_sequences
+                self.hwords = all_word_Xlengths
+                self.sequences = all_word_sequences[this_word]
+                self.X, self.lengths = all_word_Xlengths[this_word]
+                self.this_word = this_word
+                self.n_constant = n_constant
+                self.min_n_components = min_n_components
+                self.max_n_components = max_n_components
+                self.random_state = random_state
+                self.verbose = verbose
 
     def select(self):
         raise NotImplementedError
@@ -77,16 +76,45 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        print(self.words)
-        #print(self.hwords)
-        #print(self.sequences)
-        #print(self.X, self.lengths)
-        #print(self.this_word)
-        #print(self.n_constant)
-        #print(self.min_n_components)
-        #print(self.max_n_components)
-        #print(self.random_state)
-        #print(self.verbose)
+        #setting starting parameters
+        best_num_states = 0
+        score = 1e400
+        N = self.X.shape[0]
+        #iterate through all possible number of sates given in the beginning
+        for state in range(self.min_n_components, self.max_n_components+1):
+            
+            try:
+                #creating our HMM with current number of satets
+                best_model_yet = self.base_model(state)
+                #getting score for given word
+                logL = best_model_yet.score(self.X, self.lengths)
+               
+                #calculating p - number of free parameters in HMM can be hard to find
+                #These two posts helped me to get my head around finding those free params and what free params in HMM are
+                #https://stats.stackexchange.com/questions/12341/number-of-parameters-in-markov-model
+                #https://discussions.udacity.com/t/verifing-bic-calculation/246165/2
+                p = (state * state) + ( 2 * state * N )- 1
+
+                #Log of number of samples
+                logN = np.log(N) 
+                #Calculating Bayesian information criteria used to determine which model is best
+                BIC = -2 * logL + p * logN
+                
+                #comparing current best with new BIC
+                if BIC < score:
+                    score = BIC
+                    best_num_states = state
+                
+                
+            except Exception:
+                pass
+        
+        #if we couldn't calculate better number of states we set 3 as default
+        if best_num_states == 0:
+            best_num_states = 3
+
+        #return HMM model with the best number of states
+        return self.base_model(best_num_states)
 
 
 class SelectorDIC(ModelSelector):
@@ -100,9 +128,58 @@ class SelectorDIC(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        
+        #Firstly lets create loop to tet models for every sigle number of components in range
+        dic = -1e400 #-inf
+        states_hmm_best = 0 
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        #going through all possible number of states for our HMM from given range at the beginning
+        for state in range(self.min_n_components, self.max_n_components+1):
+            
+            logPofSum = 0
+            M = 0 #M is number of samples in sum 
+            #Calculating SUM of all probs from the model
+            try:
+                #creating our model with current number of states
+                model = self.base_model(state)
+            except Exception:
+                pass
+
+            #because we are summing over all words except one that we are interested in we need to set checking flag
+            flag = self.this_word
+            for word in self.words:
+                #Here we are makeing sure that we are summing over all but i (i = this_word)
+                if word != flag:
+                    
+                    current_X, current_len = self.hwords[word]
+                    
+                    M += 1
+                    try:
+                        #increasing sum for the LogLikelihood of model given current word
+                        logPofSum += model.score(current_X, current_len)
+                    except:
+                        pass
+             
+            # Until this moment we have everything except model score for word that we want to know
+            try:
+                #getting score of the model given this word
+                logL = model.score(self.X, self.lengths)
+                #getting current_score with formula for DIC
+                current_score = logL - (1/(M-1))*logPofSum
+            except Exception:
+                #in the case that HMM couldn't calculate logL, set current_score to 0 which will prevent Selector to change the current best model
+                current_score = 0
+            
+            
+            #if current model score is worse than newly calculated score we simply change models                        
+            if current_score > dic:
+                dic = current_score
+                states_hmm_best = state
+        #when we converge (find the best number of states for our HMM) we return number of HMM with that number of states
+        return self.base_model(states_hmm_best)
+                
+          
+                
 
 
 class SelectorCV(ModelSelector):
@@ -113,5 +190,54 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        #starting with default parameters (worst possible)
+        best_num_states = 0
+        best_score = -1e400
+        #n splits is variable which is helping us to determine what number of splits to find, it is going to be min of 3 and number of samples
+        n_splits = min(len(self.sequences), 3)
+
+        #going through all possible states for our HMM
+        for state in range(self.min_n_components, self.max_n_components+1):
+
+            #creating our HMM with that particular number of states
+            try:
+                #creating our model with current number of states
+                model = self.base_model(state)
+            
+
+                #creating KFold object with n_splits as the number of splits to perform on our data
+                folds = KFold(n_splits)
+                #counted is used for averaging our score
+                counter = 0 
+                avg_score = 0
+
+                #geting indices for train set and test set for each split 
+                for cv_train_idx, cv_test_idx in folds.split(self.sequences):
+
+                    #getting data for training and testing
+                    X_train, X_train_len = combine_sequences(cv_train_idx, self.sequences)
+                    X_test, X_test_len =  combine_sequences(cv_test_idx, self.sequences)
+
+                    #training model on those folds which are ment to be for training
+                    model = model.fit(X_train, X_train_len)
+                    #getting log likelihood for testing data
+                    logL = model.score(X_test, X_test_len)
+
+                    avg_score += logL
+                    counter += 1
+                   
+                #getting average score for current model
+                avg_score_fin = avg_score / counter
+
+                
+                if avg_score_fin > best_score:
+                    best_score = avg_score_fin
+                    best_num_states = state
+            
+            except Exception:
+                pass
+
+        if best_num_states == 0:
+            best_num_states = 3
+
+        return self.base_model(best_num_states)
--- my_recognizer.py
+++ my_recognizer.py
@@ -22,4 +22,37 @@ def recognize(models: dict, test_set: SinglesData):
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+    
+    #get all words for comparing
+    words = models.keys()
+
+    #get all words that we are going to test our model on, 
+    #we would like to have the biggest prob on word which is also our word from words set
+    all_words = test_set.get_all_Xlengths()
+
+    #run through all words 
+    for _, data_for_test in all_words.items():
+
+      #getting data for testing word
+      X, lengs = data_for_test
+      
+
+      our_dics = {}
+      #running through all words that we use for testing accuracy of our HMMs
+      for word in words:
+
+        model = models[word]
+        try:
+          #getting log likelihood of the test word 
+          our_dics[word] = model.score(X, lengs)
+        except:
+          pass
+
+      #this is list of all words tested for each word in our words list
+      probabilities.append(our_dics)
+
+    #getting word which has the biggest probability in each dict.
+    #we want the key word to be the same as our word which was predicted by HMM
+    guesses = [max(prob, key=prob.get) for prob in probabilities]
+
+    return probabilities, guesses
